3773e07fab00ff0aeb1314e5550dc372f8371750
konstantine <konstantine@UNICORN>
2016-11-10 18:44:24 -0800

    Add project files.

---
+++ PaketRepro/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaketRepro
+{
+    public class Class1
+    {
+    }
+}
