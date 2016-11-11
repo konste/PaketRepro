2b2c30e992e1ea9fa6d880472cc65ac9c25d43b5
konstantine <konstantine@UNICORN>
2016-11-10 18:54:58 -0800

    First commit.

---
+++ LibProject/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibProject
+{
+    public class Class1
+    {
+    }
+}
---
+++ PaketRepro.cmd
@@ -0,0 +1,2 @@
+paket pack templatefile LibProject\paket.template buildplatform AnyCPU output NuGet
+paket pack templatefile PaketRepro\paket.template buildplatform AnyCPU output NuGet
